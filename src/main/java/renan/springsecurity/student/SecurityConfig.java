0eb8894d9d465e880f95a642e8cf66d2969eda3c
Renan Bueno <renanbuenov@hotmail.com>
2021-12-07 00:55:18 -0300

    Método UserDetailsService construído e instanciado.

--- src/main/java/renan/springsecurity/student/SecurityConfig.java
+++ src/main/java/renan/springsecurity/student/SecurityConfig.java
@@ -1,8 +1,13 @@
 package renan.springsecurity.student;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -17,5 +22,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()     //No qual devem ser autenticadas
                 .and()               //E...
                 .httpBasic();        //E o mecanismo de autenticação da página será o básico (janela pop-up).
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() { //Método que irá especificar username e password do user.
+        UserDetails renanBueno = User.builder()
+                .username("renan")
+                .password("1234")
+                .roles("ROLE_STUDENT")
+                .build();
+        return new InMemoryUserDetailsManager(renanBueno); //Retorna o user "construído", mas sem o encodar a senha.
     }
 }
