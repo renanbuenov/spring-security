9805c89318992be828710905a2de65fee6ea9fda
Renan Bueno <renanbuenov@hotmail.com>
2021-12-06 21:41:32 -0300

    Definido exceções do tipo request para as autenticações.

--- src/main/java/renan/springsecurity/student/SecurityConfig.java
+++ src/main/java/renan/springsecurity/student/SecurityConfig.java
@@ -11,6 +11,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests() //Autorizar as requisições.
+                .antMatchers("/", "index", "/cs/*", "/js/*")  //Permitir todas as request que estão
+                .permitAll()                                            //definidas entre parênteses.
                 .anyRequest()        //Qualquer requisição
                 .authenticated()     //No qual devem ser autenticadas
                 .and()               //E...
