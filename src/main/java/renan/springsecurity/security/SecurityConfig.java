6d545fbc730326374e17a7d784d920a464ad2ac9
Renan Bueno <renanbuenov@hotmail.com>
2021-12-10 15:09:28 -0300

    Implementação da JWT.

--- src/main/java/renan/springsecurity/security/SecurityConfig.java
+++ src/main/java/renan/springsecurity/security/SecurityConfig.java
@@ -2,30 +2,41 @@ package renan.springsecurity.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import renan.springsecurity.auth.UserService;
+import renan.springsecurity.jwt.JwtConfig;
+import renan.springsecurity.jwt.JwtTokenVerifier;
+import renan.springsecurity.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 
-import java.util.concurrent.TimeUnit;
+import javax.crypto.SecretKey;
 
-import static renan.springsecurity.security.UserRole.*;
+import static renan.springsecurity.security.UserRole.STUDENT;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
+    private final UserService userService;
+    private final SecretKey secretKey;
+    private final JwtConfig jwtConfig;
 
     @Autowired
-    public SecurityConfig(PasswordEncoder passwordEncoder) {
+    public SecurityConfig(PasswordEncoder passwordEncoder,
+                          UserService userService,
+                          SecretKey secretKey,
+                          JwtConfig jwtConfig) {
         this.passwordEncoder = passwordEncoder;
+        this.userService = userService;
+        this.secretKey = secretKey;
+        this.jwtConfig = jwtConfig;
     }
 
     @Override
@@ -33,56 +44,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
 //                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
                 .csrf().disable()
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager(), jwtConfig, secretKey))
+                .addFilterAfter(new JwtTokenVerifier(secretKey, jwtConfig), JwtUsernameAndPasswordAuthenticationFilter.class)
                 .authorizeRequests() //Autorizar as requisições.
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()  //Permitir todas as request que estão definidas entre parênteses.
                 .antMatchers("/api/**").hasRole(STUDENT.name())
-                .anyRequest()       //Qualquer requisição
-                .authenticated()     //No qual devem ser autenticadas
-                .and()               //E...
-                .formLogin()
-                    .loginPage("/login")
-                    .permitAll()
-                    .defaultSuccessUrl("/courses", true)
-                    .usernameParameter("username")
-                    .passwordParameter("password")
-                .and()
-                .rememberMe()
-                    .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
-                    .key("algomuitoseguro")
-                .and()
-                .logout()
-                    .logoutUrl("/logout")
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
-                    .clearAuthentication(true)
-                    .invalidateHttpSession(true)
-                    .deleteCookies("JSESSIONID", "remember-me", "Idea-4637f4b6", "XSRF-TOKEN")
-                    .logoutSuccessUrl("/login");        //E o mecanismo de autenticação da página será o básico (janela pop-up).
+                .anyRequest()
+                .authenticated();
     }
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
     @Bean
-    protected UserDetailsService userDetailsService() { //Método que irá especificar username e password do user.
-        UserDetails renanBueno = User.builder()
-                .username("renan")
-                .password(passwordEncoder.encode("1234"))
-//                .roles(ADMIN.name())
-                .authorities(ADMIN.getGrantedAuthorities())
-                .build();
-        UserDetails fernandaMarie = User.builder()
-                .username("fernanda")
-                .password(passwordEncoder.encode("fernanda"))
-//                .roles(STUDENT.name())
-                .authorities(ADMIN_TRAINEE.getGrantedAuthorities())
-                .build();
-        UserDetails rafaelBueno = User.builder()
-                .username("rafael")
-                .password(passwordEncoder.encode("fernanda"))
-//                .roles(ADMIN_TRAINEE.name())
-                .authorities(STUDENT.getGrantedAuthorities())
-                .build();
-        return new InMemoryUserDetailsManager(
-                renanBueno,
-                fernandaMarie,
-                rafaelBueno); //Retorna o user "construído".
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(userService);
+        return provider;
     }
 }
